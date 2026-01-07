78190958ea4ef21fe40d9f1bb3e734ca7454619e
Hannah Virgo <HannahGMMCVirgo95@gmail.com>
2026-01-07 05:27:15 +0000

    Created a new branch called: finalRefactorandTesting
    - Blackbox testing for end-to-end and equivalence partition has been completed.
    
    - JUnit testing has been completed. Just need to get clarification on it before final commit is done.
    
    - Make setTheProduct public for the JUnit testing customermodel.

--- src/test/java/ci553/happyshop/client/customer/CustomerModelTest.java
+++ src/test/java/ci553/happyshop/client/customer/CustomerModelTest.java
@@ -13,14 +13,28 @@ class CustomerModelTest {
     @Test
     void makeOrganisedTrolley() {
         CustomerModel cm = new CustomerModel();
+
         Product p = new Product("0001", "TV", "0001.jpg", 12.01, 100);
         cm.setTheProduct(p);
-        cm.makeOrganisedTrolley();
-        cm.makeOrganisedTrolley();
-        cm.makeOrganisedTrolley();
-        ArrayList<Product> tro = cm.getTrolley();
-        assertEquals(1, tro.size());
-        assertEquals(3, tro.get(0).getOrderedQuantity());
 
+        // User clicking 'add to trolley' 5 times
+        cm.makeOrganisedTrolley();
+        cm.makeOrganisedTrolley();
+        cm.makeOrganisedTrolley();
+        cm.makeOrganisedTrolley();
+        cm.makeOrganisedTrolley();
+
+        ArrayList<Product> tro = cm.getTrolley();
+
+        // Verify the size list is 1
+        assertEquals(1, tro.size(), "The list size should be 1, due to merged items");
+
+        // Verify the quantity has updated to 5 correctly
+        assertEquals(5, tro.get(0).getOrderedQuantity(), "The quantity should match 5 items");
+
+        System.out.println("makeOrganisedTrolley has passed!");
     }
 }
+
+
+
